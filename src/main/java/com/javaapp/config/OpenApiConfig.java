5337107c2620e0c4e09ca196a28b0161ef74bb09
Ali Bozdogan <ali.h.bozdogan@gmail.com>
2021-10-29 13:24:22 -0500

    Web Security Changes 4

--- src/main/java/com/javaapp/config/OpenApiConfig.java
+++ src/main/java/com/javaapp/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.javaapp.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +21,7 @@ public class OpenApiConfig {
         securitySchemeItem.setBearerFormat("JWT");
         securitySchemeItem.setIn(SecurityScheme.In.HEADER);
         securitySchemeItem.setName("Authorization");
-        io.swagger.v3.oas.models.info.Info infoVersion = new io.swagger.v3.oas.models.info.Info().title("Java App Info").version("snapshot");
+        Info infoVersion = new Info().title("Java App Info Ticketing Application").version("snapshot");
         SecurityRequirement securityItem = new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read","write"));
 
         return new OpenAPI()
