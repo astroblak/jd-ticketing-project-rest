a79bbbe30eb0fb61967dbad0886fb46e670867a1
Ali Bozdogan <ali.h.bozdogan@gmail.com>
2021-10-29 12:52:44 -0500

    Web Security Changes 4

--- src/main/java/com/javaapp/controller/LoginController.java
+++ src/main/java/com/javaapp/controller/LoginController.java
@@ -1,20 +1,56 @@
 package com.javaapp.controller;
 
+import com.javaapp.dto.UserDTO;
+import com.javaapp.entity.ResponseWrapper;
+import com.javaapp.entity.User;
+import com.javaapp.entity.common.AuthenticationRequest;
+import com.javaapp.exception.TicketingProjectException;
+import com.javaapp.mapper.MapperUtil;
+import com.javaapp.service.UserService;
+import com.javaapp.util.JWTUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LoginController {
-	
-	@RequestMapping(value = {"/login","/"})
-	public String login(){
 
-		return "login";
+	private AuthenticationManager authenticationManager;
+	private UserService userService;
+	private MapperUtil mapperUtil;
+	private JWTUtil jwtUtil;
+
+	public LoginController(AuthenticationManager authenticationManager, UserService userService, MapperUtil mapperUtil, JWTUtil jwtUtil) {
+		this.authenticationManager = authenticationManager;
+		this.userService = userService;
+		this.mapperUtil = mapperUtil;
+		this.jwtUtil = jwtUtil;
 	}
-	
-	@RequestMapping("/welcome")
-	public String welcome(){
-		return "welcome";
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<ResponseWrapper>doLogin(@RequestBody AuthenticationRequest authenticationRequest) throws TicketingProjectException {
+
+		String password = authenticationRequest.getPassword();
+		String username = authenticationRequest.getUsername();
+
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username,password);
+		authenticationManager.authenticate(authentication);
+
+		UserDTO foundUser = userService.findByUserName(username);
+		User convertedUser = mapperUtil.convert(foundUser,new User());
+
+		if(!foundUser.isEnabled()){
+			throw new TicketingProjectException("Please verify your user");
+		}
+
+		String jwtToken= jwtUtil.generateToken(convertedUser);
+
+		return ResponseEntity.ok(new ResponseWrapper("Login Successful",jwtToken));
 	}
 
 }
